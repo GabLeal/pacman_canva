5af7c5d7c83dca474247460d43cb3716521b22e9
GabLeal <gabriel.leal@compasso.com.br>
2022-08-19 21:18:29 -0300

    feat: criando funçoes para separar a forma como o desenho é construido

--- lib/pacman/pac_man_canva.dart
+++ lib/pacman/pac_man_canva.dart
@@ -11,27 +11,40 @@ class PacManCanva extends CustomPainter {
     this.foodValue,
   );
 
-  @override
-  void paint(Canvas canvas, Size size) {
-    final food = Paint()..color = const Color(0xFFfdff00);
+  Paint get foodPaint {
+    return Paint()..color = const Color(0xFFfdff00);
+  }
 
-    canvas.drawCircle(
-      Offset(
-        foodValue + 20,
-        size.height / 2,
-      ),
-      11,
-      food,
-    );
-    var paint1 = Paint()
+  Paint get bodyPaint {
+    return Paint()
       ..color = const Color(0xFFfdff00)
       ..style = PaintingStyle.fill;
+  }
 
+  Paint get strokeBodyPaint {
+    return Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3;
+  }
+
+  Paint get eyesPaint {
+    return Paint()..color = Colors.black;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    drawFood(canvas, size, foodPaint);
+    drawBodyPacman(canvas, size, bodyPaint);
+    drawBodyPacman(canvas, size, strokeBodyPaint);
+    drawEyes(canvas, size, eyesPaint);
+  }
+
+  void drawBodyPacman(Canvas canvas, Size size, Paint paint) {
     //draw arc
     final rect = Rect.fromLTWH(0, 0, size.width, size.width);
 
-    //var radianos:Number = graus * (Math.PI / 180);
-
+    //radianos = graus * (PI / 180);
     var startAngle = value / 100;
     var sweepAngle = (360 - value / 2) * pi / 180 - value / 100;
     const useCenter = true;
@@ -41,30 +54,30 @@ class PacManCanva extends CustomPainter {
       startAngle.toDouble(), //radians
       sweepAngle.toDouble(), //radians
       useCenter,
-      paint1,
+      paint,
     );
-    var paint11 = Paint()
-      ..color = Colors.black
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 3;
+  }
 
-    canvas.drawArc(
-      rect,
-      startAngle.toDouble(), //radians
-      sweepAngle.toDouble(), //radians
-      useCenter,
-      paint11,
+  void drawFood(Canvas canvas, Size size, Paint paint) {
+    canvas.drawCircle(
+      Offset(
+        foodValue + 20,
+        size.height / 2,
+      ),
+      11,
+      paint,
     );
+  }
+
+  void drawEyes(Canvas canvas, Size size, Paint paint) {
     var eyesMove = value / 10;
-    final eyes = Paint()..color = Colors.black;
-
     canvas.drawCircle(
       Offset(
         size.width / 1.8 - eyesMove,
         size.height / 4 - eyesMove,
       ),
       10,
-      eyes,
+      eyesPaint,
     );
   }
 
